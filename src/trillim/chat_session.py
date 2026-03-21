ef26b150d1ddd52b2b25b0470dae1fba04f3db19
Vineet-V <vineetv314@gmail.com>
2026-03-20 17:18:46 -0700

    Delete files for refactoring

--- src/trillim/chat_session.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026 Trillim. Licensed under the MIT License. See LICENSE.
-"""Public ChatSession export for multi-turn LLM conversations.
-
-Use ``llm.session(...)`` to create instances.
-"""
-
-from .server._llm import ChatSession
-
-__all__ = ["ChatSession"]
